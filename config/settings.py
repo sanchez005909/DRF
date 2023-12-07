3729a74d232f3fff7ef5ecde42cc399e845bfc0a
alex <sanchez005909@gmail.com>
2023-12-07 14:32:15 +0300

    install django-filters

--- config/settings.py
+++ config/settings.py
@@ -45,11 +45,6 @@ INSTALLED_APPS = [
 
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ),
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
