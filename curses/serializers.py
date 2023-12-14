09c724985ef05dc3ef736ef3761637b95e9fdbe8
alex <sanchez005909@gmail.com>
2023-12-14 20:41:17 +0300

    create serializer for Subscription

--- curses/serializers.py
+++ curses/serializers.py
@@ -1,22 +1,30 @@
 from rest_framework import serializers
-from curses.models import Curs, Lesson
+from curses.models import Curs, Lesson, Subscription
+from curses.validators import URLValidator
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Subscription
+        fields = ['user', 'course']
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = ['id', 'title', 'description', 'owner']
+        fields = ['id', 'title', 'description', 'url_video', 'owner']
+        validators = [URLValidator(field='url_video')]
 
 
 class CursSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField(source='lessons.count')
+    subscribers = SubscriptionSerializer(source='subscription_set', many=True)
+    lessons_ = LessonSerializer(source='lessons', many=True)
 
     class Meta:
         model = Curs
-        fields = ['id', 'title', 'description', 'owner', 'lessons_count', 'lessons']
+        fields = ['id', 'title', 'description', 'owner', 'lessons_count', 'lessons_', 'subscribers']
 
     def get_lessons_count(self, instance):
         return instance.lessons.count()
-
-
-
